0780593509200b43e8cf58591bc34c300a2d4f96
MartinBKock <martin_kock@me.com>
2023-05-15 08:54:56 +0200

    Initial Commit

---
+++ ISpecialiseringsopgaveMBK/ISpecialiseringsopgaveMBKApp.swift
@@ -0,0 +1,17 @@
+//
+//  ISpecialiseringsopgaveMBKApp.swift
+//  ISpecialiseringsopgaveMBK
+//
+//  Created by Martin Kock on 15/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct ISpecialiseringsopgaveMBKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
